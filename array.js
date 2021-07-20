a120a603e37b9b77ba7c5a1dcab982028f5caa0c
Samandar <86640731+samandaru@users.noreply.github.com>
2021-07-21 00:31:24 +0900

    object

---
+++ array.js
@@ -0,0 +1,51 @@
+var students = [
+    {
+        id: 1,
+        year: 1999,
+        name: 'Bekzod',
+        surname: 'Abdulkhaev',
+
+    }  , 
+    {
+         id: 2,
+         year: 2000,
+         name: 'Akbarjon',
+         surname: 'Urasboyev',
+    
+    },
+    {
+        id: 3,
+        year: 1998,
+        name: 'Barkamol',
+        surname: 'Olimjonov',
+        
+    },
+    {
+        id: 4,
+        year: 2001,
+        name: 'Jamshid',
+        surname: 'Karimkulov',
+    },
+    {
+        id: 5,
+        year: 2001,
+        name: 'Mirabdulla',
+        surname: 'Toshtanov',
+                
+    },
+    {
+        id: 6,
+        year: 2002,
+        name: 'Mirzohid',
+        surname: 'Boboqulov',
+                    
+    }
+   
+]
+ students.map((value)=>{
+    
+    console.log(students.sort());
+    console.log(value.name.sort());
+ });
+//  console.log(students.sort());
+
